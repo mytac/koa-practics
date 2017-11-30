2a3e797a86b70af17a7bd4fc65337280ec06de45
张君卓 <my_tac@126.com>
2017-11-30 17:59:02 +0800

    环境配置

--- demo.js
+++ demo.js
@@ -1,13 +1,27 @@
 const koa=require('koa')
 const router=require('koa-router')
 
+const config=require('./config/config')
+
 const app=koa()
 
+app.use(function *(next){
+    // 注入config配置信息
+    if(!this.config){
+        this.config=config
+    }
+    yield next
+})
 app.use(router(app))
 
 app.get('/',function *(next){
     //首页
     this.body='首页'
+})
+
+app.get('/config',function *(next){
+    // config注入中间件，配置信息
+    this.body=this.config.env
 })
 
 app.param('id',function *(id,next){
